d5f194f3c0279eefc855062bc83bdd2565b4a154
Ricardo Diaz <rdiaz@jacidi.com>
2022-03-24 14:22:17 -0400

    initial commit

---
+++ src/test/java/com/zenithdev/socially/SociallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zenithdev.socially;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
